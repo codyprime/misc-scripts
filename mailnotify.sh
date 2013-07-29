3f4faec52b1250db1ba29b6b93a2673990497e7f
Jeff Cody <jcody@redhat.com>
2013-07-29 16:40:16 -0400

    Simple bash script to notify when new email has arrived.
    This is intended to be used with offlineimap (with or without mutt).
    
    You can specify which mailbox directories to monitor for new mail,
    and the mailbox dirs are efficiently monitored with inotifywait.
    
    Events monitored are 'create' and 'moved_to', which should work for
    offlineimap.
    
    Notifications are sent via notify-send (part of libnotify), which should
    work with XFCE, KDE, GNOME 2/3, Unity, etc..
    
    You may need to install the inotify-tools package for inotifywait.

---
+++ mailnotify.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# Very simple, and somewhat brain-dead, script to notify when new mail
+# has arrived, via the default notification system of your desktop.
+# 
+# This makes use of inotify-tools.  On a Fedora-based system, install
+# via 'yum install inotify-tools'
+#
+# This is distributed as GPLv2; See LICENSE file for details.
+
+
+# Use full paths, and include the "new/" diretory
+# Entries should look this this, with each mailbox directory on
+# its own line (sample watch file content):
+# /home/Jeff.Cody/maildata/mail/redhat/INBOX/new
+# /home/Jeff.Cody/maildata/mail/redhat/qemu-devel/new
+# /home/Jeff.Cody/maildata/mail/redhat/important/new
+MAIL_WATCH_FILE="/home/Jeff.Cody/mailconfig/watched-dirs.txt"
+# The icon you would like displayed.  You Gnomes might want to look
+# in /usr/share/icons/gnome
+ICON="/usr/share/icons/oxygen/64x64/status/mail-unread-new.png"
+
+# milliseconds to display notification
+DISPLAY_MS=5000
+
+# This will loop until killed
+(
+while read ievent
+do
+    path=`echo -n $ievent|sed -e "s/[^/]*$//"`
+    # this regex is a bit broken in that assumes the filename itself
+    # won't have MOVED_TO or CREATE in it - which for an offline mailbox
+    # file, it won't
+    filename=`echo -n $ievent|sed -e "s/.*\b\(MOVED_TO\|CREATE\)\b //"`
+    mailbox=${path:0:-5}  # get rid of "new/"
+    mailbox=${mailbox##/*/} # get rid of /*/
+    from=`grep -m 1 ^From: "${path}${filename}"`
+    subject=`grep -m 1 ^Subject: "${path}${filename}"`
+    notify-send -t $DISPLAY_MS -i $ICON "New mail in $mailbox" "$from\n$subject"
+done < <(inotifywait -m -e create -e moved_to --fromfile ${MAIL_WATCH_FILE})
+)2>/dev/null
